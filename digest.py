b62f7a552cc3d1d17ff845c9f2e12c134f9ee390
edd19 <eddyndizera@gmail.com>
2016-10-14 14:18:51 +0200

    read data from html

--- digest.py
+++ digest.py
@@ -1,1 +1,21 @@
-#launcher
+#!/usr/bin/env python3
+# Extract ingredients from an html recipe page and output it into a yaml file.
+# Usage: - ./digest.py path/to/input.html path/to/output.yaml
+
+import sys
+import html_parser
+
+
+def main(args):
+    input = args[1]
+    output = args[2]
+
+    extract_ingredients(input)
+
+
+def extract_ingredients(input):
+    with open(input, "r") as input_file:
+        html_parser.parse_all_file(input_file)
+
+arguments = sys.argv
+main(arguments)
--- html_parser.py
+++ html_parser.py
@@ -1,4 +1,15 @@
-from HTMLParser import HTMLParser
+#!/usr/bin/env python3
+# Parse an html to extract ingredients.
 
-class MyHTMLParser(HTMLParser):
-    s
+from html import parser
+
+
+class IngredientsHTMLParser(parser.HTMLParser):
+    def handle_data(self, data):
+        print("Data: " + data) # TODO check if is ingredient
+
+
+def parse_all_file(file_to_parse):
+    ingredients_parser = IngredientsHTMLParser()
+    for line in file_to_parse:
+        ingredients_parser.feed(line)
