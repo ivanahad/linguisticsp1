8cdbabdb686c28fb02aa725c334bee202c9f8664
edd19 <eddyndizera@gmail.com>
2016-10-16 16:47:48 +0200

    added only element regex

--- regex.py
+++ regex.py
@@ -2,16 +2,28 @@
 import re
 
 
-#classic = r"-(?P<quantity>\d+(\\d+)?) (?P<unit>\w+) (?P<element>.+)"
+simple = r"-?(?P<quantity>\d+(\/\d+)?) (?P<unit>\w+\.?) (?P<element>.+)"  # quantity - unit - element
+only_element = r'-?(?P<element>.+)'  # element
+or_regex = r"|"
 
-classic = r"-?(?P<quantity>\d+(\/\d+)?) (?P<unit>\w+\.?) (?P<element>.+)"
 
-def simpleregex(line):
-    p = re.compile(classic)
-    match = p.match(line)
-    if not match:
-        return None
-    result = match.group()
-    return {"ingredient": match.group("element"), "quantity": match.group("quantity"), "unit": match.group("unit"),
-            "line": line}
+def match(line, pattern):
+    p = re.compile(pattern)
+    return p.match(line)
+
+
+def simple_regex(line):
+    result = match(line, simple)
+    return {"ingredient": result.group("element"), "quantity": result.group("quantity"),
+            "unit": result.group("unit")} if result else None
+
+
+def only_element_regex(line):
+    result = match(line, only_element)
+    return {"ingredient": result.group("element")} if result else None
+
+
+def advanced_regex(line):
+    result = simple_regex(line)
+    return result if result else only_element_regex(line)
 
